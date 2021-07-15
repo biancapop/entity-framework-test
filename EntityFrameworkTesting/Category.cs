36505576ee15cb01521facb50680911faa4815ab
Bianca <popescucarmenbianca@gmail.com>
2021-07-15 10:40:49 +0200

    database context added and classes

---
+++ EntityFrameworkTesting/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace EntityFrameworkTesting
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
