a0836b9720fbc9835a9b5cd350f6c0c56a1803f1
Bianca <popescucarmenbianca@gmail.com>
2021-07-16 10:36:08 +0200

    cambios en EF

--- EntityFrameworkTesting/Task.cs
+++ EntityFrameworkTesting/Task.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkTesting
 {
-    class Task
+    public class Task
     {
         public int Id { get; set; }
         public string Name { get; set; }
---
+++ EntityFrameworkTesting/TaskRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTesting
+{
+    public interface ITaskRepository{
+        void CreateTask(Task task);
+
+    }
+
+
+    class TaskRepository:ITaskRepository
+    {
+        private DatabaseContext _databaseContext;
+
+        public TaskRepository(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public void CreateTask(Task task)
+        {
+            if (task != null) {
+                _databaseContext.Tasks.Add(task);
+                _databaseContext.SaveChanges();
+            }
+        }
+    }
+}
